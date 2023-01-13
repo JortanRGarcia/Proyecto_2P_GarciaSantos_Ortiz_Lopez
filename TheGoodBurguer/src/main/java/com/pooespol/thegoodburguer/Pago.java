9527fd8186a46d16753e0829e1f24af3ad1fdb84
cecilitaortiz <ceelorti@espol.edu.ec>
2023-01-13 18:06:37 -0500

    prueba

---
+++ TheGoodBurguer/src/main/java/com/pooespol/thegoodburguer/Pago.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pooespol.thegoodburguer;
+
+/**
+ *
+ * @author Cecy
+ */
+public class Pago {
+    
+}
