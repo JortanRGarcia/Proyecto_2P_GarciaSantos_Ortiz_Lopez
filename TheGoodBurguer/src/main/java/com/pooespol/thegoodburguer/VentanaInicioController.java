b90029ad74813eb5c00aef0b7175a8fd554b5243
Usuario <Usuario@DESKTOP-I2TJP60>
2023-01-26 18:10:09 -0500

    Creacion de las primeras ventanas FXML

---
+++ TheGoodBurguer/src/main/java/com/pooespol/thegoodburguer/VentanaInicioController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.pooespol.thegoodburguer;
+
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class VentanaInicioController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+
+    @FXML
+    private TextField tf1;
+    
+    @FXML
+    private TextField tf2;
+    
+    @FXML
+    private ImageView imgv1;
+    
+    @FXML
+    private ImageView imgv2;
+    
+    @FXML 
+    private Button btnIngresar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        cargarImagenes();
+         
+        
+    }    
+    public void cargarImagenes(){
+        try{
+            Image img = new Image(new FileInputStream("src/main/resources/images/pantallaInicial.png"));
+            imgv1.setImage(img);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        try{
+            Image img = new Image(new FileInputStream("src/main/resources/images/BurguerKawai.png"));
+            imgv2.setImage(img);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
--- TheGoodBurguer/src/main/java/com/pooespol/thegoodburguer/VentanaPedidoController.java
+++ TheGoodBurguer/src/main/java/com/pooespol/thegoodburguer/VentanaPedidoController.java
@@ -6,23 +6,36 @@
 // Jortan
 package com.pooespol.thegoodburguer;
 
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
  *
  * @author jorgs
  */
+
+
 public class VentanaPedidoController implements Initializable {
 
     /**
      * Initializes the controller class.
      */
+  
+   
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+     
+        
     }    
     
 }
