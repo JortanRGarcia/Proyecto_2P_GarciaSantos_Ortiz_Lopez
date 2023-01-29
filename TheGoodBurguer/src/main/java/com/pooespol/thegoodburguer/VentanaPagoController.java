a5efb03a88fa9176e44a9240b67f6ccbbeb197c6
carloslopezdiaz <carldiaz@espol.edu.ec>
2023-01-29 17:04:39 -0500

    Modelo base de Pago Controller

--- TheGoodBurguer/src/main/java/com/pooespol/thegoodburguer/VentanaPagoController.java
+++ TheGoodBurguer/src/main/java/com/pooespol/thegoodburguer/VentanaPagoController.java
@@ -11,6 +11,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -20,7 +27,17 @@ import javafx.scene.control.Button;
 public class VentanaPagoController implements Initializable {
 
     @FXML
+    private TextField tfdireccion;
+    @FXML
     private Button btnContinuarGracias;
+    @FXML
+    private RadioButton rbefectivo;
+    @FXML
+    private RadioButton rbtarjeta;
+    @FXML
+    private Button btnlimpiar;
+    @FXML
+    private VBox datostarjeta;
 
     /**
      * Initializes the controller class.
@@ -28,6 +45,11 @@ public class VentanaPagoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        final ToggleGroup group = new ToggleGroup();
+        rbefectivo.setToggleGroup(group);
+        rbtarjeta.setToggleGroup(group);
+        
+        
     }    
 
     @FXML
@@ -35,4 +57,65 @@ public class VentanaPagoController implements Initializable {
         App.setRoot("ventanaGracias");
     }
     
+    @FXML
+    private void detalleEfectivo(ActionEvent event) {
+        if(rbefectivo.isSelected()){
+            datostarjeta.getChildren().clear();
+            Label text=new Label();
+            text.setText("Hola");
+            datostarjeta.getChildren().add(text);
+        }
+    }
+    
+    @FXML
+    private void detalleTarjeta(ActionEvent event) {
+        if(rbtarjeta.isSelected()){
+            datostarjeta.getChildren().clear();
+            
+            HBox nombre=new HBox();
+            HBox numero=new HBox();
+            HBox caducidad=new HBox();
+            HBox cvv=new HBox();
+            nombre.setSpacing(45);
+            numero.setSpacing(10);
+            caducidad.setSpacing(5);
+            cvv.setSpacing(70);
+            
+            Label name=new Label();
+            TextField tfnombre=new TextField();
+            name.setText("Nombre: ");
+            nombre.getChildren().addAll(name,tfnombre);
+            
+            Label num=new Label();
+            TextField tfnum=new TextField();
+            num.setText("Numero tarjeta: ");
+            numero.getChildren().addAll(num,tfnum);
+            
+            Label cad=new Label();
+            TextField tfcad=new TextField();
+            cad.setText("Fecha caducidad: ");
+            caducidad.getChildren().addAll(cad,tfcad);
+            
+            Label c=new Label();
+            TextField tfc=new TextField();
+            c.setText("CVV: ");
+            cvv.getChildren().addAll(c,tfc);
+            
+            Label text=new Label();
+            text.setText("Hola");
+            
+            datostarjeta.getChildren().addAll(nombre,numero,caducidad,cvv,text);
+            
+            btnlimpiar.setOnMouseClicked((MouseEvent e) -> {
+                tfnombre.clear();
+                tfnum.clear();
+                tfcad.clear();
+                tfc.clear();   
+            });
+            
+            
+        }
+        
+    }
+   
 }
